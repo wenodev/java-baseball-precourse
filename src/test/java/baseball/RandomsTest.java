70122e73640a7e9f6ff5b86dd347065de3333288
wenodev <logicalstark1105@gmail.com>
2022-04-18 20:23:53 +0900

    style(RandomsTest.java): 불필요한 import를 제거합니다.

--- src/test/java/baseball/RandomsTest.java
+++ src/test/java/baseball/RandomsTest.java
@@ -3,7 +3,6 @@ package baseball;
 import camp.nextstep.edu.missionutils.Randoms;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
-import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
