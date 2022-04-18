15820a6c7645a18f445a6403af7bb62fb7ef4ee6
wenodev <logicalstark1105@gmail.com>
2022-04-18 20:17:48 +0900

    test(BallTest.java, BallsTest): 테스트 케이스를 추가합니다.

--- src/test/java/baseball/BallTest.java
+++ src/test/java/baseball/BallTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.params.provider.CsvSource;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class BallTest {
+    @DisplayName("position과 number로 구성됩니다.")
     @ParameterizedTest
     @CsvSource(value = {"1,1", "2,2"})
     void construct(int position, int number){
@@ -21,5 +22,38 @@ public class BallTest {
     @Test
     void equals(){
         assertThat(new Ball(1,1)).isEqualTo(new Ball(1,1));
+    }
+
+    @DisplayName("같은 Ball이라면 STRIKE 입니다.")
+    @Test
+    void judge_strike(){
+        Ball computerBall = new Ball(1,1);
+        Ball playerBall = new Ball(1,1);
+
+        BallStatus ballStatus = computerBall.compare(playerBall);
+
+        assertThat(ballStatus).isEqualTo(BallStatus.STRIKE);
+    }
+
+    @DisplayName("position은 다른고 number가 같으면 BALL 입니다.")
+    @Test
+    void judge_ball(){
+        Ball computerBall = new Ball(1,1);
+        Ball playerBall = new Ball(2,1);
+
+        BallStatus ballStatus = computerBall.compare(playerBall);
+
+        assertThat(ballStatus).isEqualTo(BallStatus.BALL);
+    }
+
+    @DisplayName("position과 number 둘 다 다르면 NOTHING 입니다.")
+    @Test
+    void judge_nothing(){
+        Ball computerBall = new Ball(1,1);
+        Ball playerBall = new Ball(2,2);
+
+        BallStatus ballStatus = computerBall.compare(playerBall);
+
+        assertThat(ballStatus).isEqualTo(BallStatus.NOTHING);
     }
 }
--- src/test/java/baseball/BallsTest.java
+++ src/test/java/baseball/BallsTest.java
@@ -1,13 +1,26 @@
 package baseball;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class BallsTest {
+    @DisplayName("integer array로 생성합니다.")
     @Test
-    void construct(){
-        Balls balls = new Balls(1,2,3);
+    void construct_intArray(){
+        Balls balls = new Balls(Arrays.asList(1,2,3));
+
+        assertThat(balls.getValues().size()).isEqualTo(3);
+        assertThat(balls.getValues().get(0)).isEqualTo(new Ball(1,1));
+    }
+
+    @DisplayName("string으로 생성합니다.")
+    @Test
+    void construct_string(){
+        Balls balls = new Balls("123");
 
         assertThat(balls.getValues().size()).isEqualTo(3);
         assertThat(balls.getValues().get(0)).isEqualTo(new Ball(1,1));
