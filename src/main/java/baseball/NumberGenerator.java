0aa25d217fe6e5ccb76541ae86d681e86ce9a381
wenodev <logicalstark1105@gmail.com>
2022-04-18 21:58:11 +0900

    refactor(NumberGenerator.java): 매직넘버를 상수로 변경합니다.

--- src/main/java/baseball/NumberGenerator.java
+++ src/main/java/baseball/NumberGenerator.java
@@ -6,12 +6,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class NumberGenerator {
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 9;
+    private static final int MAX_NUMBERS_SIZE = 3;
+
+
     public List<Integer> generate() {
         List<Integer> numbers = new ArrayList<>();
         do {
-            int number = Randoms.pickNumberInRange(1,9);
+            int number = Randoms.pickNumberInRange(MIN_NUMBER,MAX_NUMBER);
             addNotDuplicateNumber(numbers, number);
-        } while (numbers.size() < 3);
+        } while (numbers.size() < MAX_NUMBERS_SIZE);
         return numbers;
     }
 
