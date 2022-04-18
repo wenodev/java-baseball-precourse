450608d226783f7c202c71d7b96fb955fae6cee9
wenodev <logicalstark1105@gmail.com>
2022-04-18 20:23:32 +0900

    style(Position.java): static과 final의 위치를 변경합니다

--- src/main/java/baseball/Position.java
+++ src/main/java/baseball/Position.java
@@ -3,8 +3,8 @@ package baseball;
 import java.util.Objects;
 
 public class Position {
-    private final static int MIN_VALUE = 1;
-    private final static int MAX_VALUE = 3;
+    private static final int MIN_VALUE = 1;
+    private static final int MAX_VALUE = 3;
     private static final String INVALID_VALUE_MESSAGE = "위치값은 1 이상 3 이하여야 합니다.";
 
     private final int value;
